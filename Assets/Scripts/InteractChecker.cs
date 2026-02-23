fb8fba8cf468380c1d006495916790577fbd8174
orangejuice0 <92806069+orangejuice0@users.noreply.github.com>
2026-02-23 13:35:20 +0000

    created Interact Checker
    Uses raycast to get tag of hit gameObject

---
+++ Assets/Scripts/InteractChecker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class InteractChecker : MonoBehaviour
+{
+    [SerializeField]private float rayDistance = 100f;
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            string hitTag = hit.collider.gameObject.tag;
+            string hitName = hit.collider.gameObject.name;
+            Debug.Log($"Hit object tag: {hitTag}\nHit object name: {hitName}");
+        }
+    }
+}
