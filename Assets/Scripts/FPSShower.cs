3d2c47a85f225a665c025016f7151da4f4802d5a
orangejuice0 <92806069+orangejuice0@users.noreply.github.com>
2026-05-13 15:36:00 +0100

    Window Mode
    Added window mode and FPX counter

---
+++ Assets/Scripts/FPSShower.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+
+public class FPSShower : MonoBehaviour
+{
+    [SerializeField]private TextMeshProUGUI fpsText;
+
+    private float deltaTime;
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+        float fps = 1.0f / deltaTime;
+
+        fpsText.text = Mathf.Ceil(fps).ToString() + " FPS";
+    }
+}
--- Assets/Scripts/Player/EscapeMenu.cs
+++ Assets/Scripts/Player/EscapeMenu.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,7 +8,71 @@ public class EscapeMenu : MonoBehaviour
 {
     [SerializeField]private InputActionReference openEscapeMenu;
     [SerializeField]private GameObject escapeMenuUI;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
     private bool isEscapeMenuOpen = false;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        List<Resolution> uniqueResolutions = new List<Resolution>();
+
+        foreach (var res in resolutions)
+        {
+            bool exists = uniqueResolutions.Any(r =>
+                r.width == res.width &&
+                r.height == res.height);
+
+            if (!exists)
+                uniqueResolutions.Add(res);
+        }
+
+        resolutions = uniqueResolutions.ToArray();
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option =
+                $"{resolutions[i].width} x {resolutions[i].height}";
+
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+    }
+
+    public void SetResolution(int index)
+    {
+        Resolution resolution = resolutions[index];
+
+        Screen.SetResolution(
+            resolution.width,
+            resolution.height,
+            isFullscreen
+                ? FullScreenMode.FullScreenWindow
+                : FullScreenMode.Windowed
+        );
+
+
+    }
 
     private void OnEnable()
     {
@@ -34,6 +101,17 @@ public class EscapeMenu : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
+    }
+
+    private bool isFullscreen = true;
+
+    public void SetFullscreen(bool value)
+    {
+        isFullscreen = value;
+
+        Screen.fullScreenMode = value
+            ? FullScreenMode.FullScreenWindow
+            : FullScreenMode.Windowed;
     }
 
     public void Quit()
